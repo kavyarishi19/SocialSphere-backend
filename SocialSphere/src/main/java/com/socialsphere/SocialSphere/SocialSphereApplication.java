b435299e004d903e9096ce0b909b5be79dc4e7c7
kavyarishi19 <forcodingpurposes19@gmail.com>
2023-07-28 19:30:22 +0530

    initial setup

---
+++ SocialSphere/src/main/java/com/socialsphere/SocialSphere/SocialSphereApplication.java
@@ -0,0 +1,13 @@
+package com.socialsphere.SocialSphere;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SocialSphereApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SocialSphereApplication.class, args);
+	}
+
+}
---
+++ SocialSphere/src/test/java/com/socialsphere/SocialSphere/SocialSphereApplicationTests.java
@@ -0,0 +1,13 @@
+package com.socialsphere.SocialSphere;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SocialSphereApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
